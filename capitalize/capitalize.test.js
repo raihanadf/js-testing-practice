ecec2e90dbad7e34bcadbca370e39c0900e4a163
Raihan <12584890+17radf@users.noreply.github.com>
2022-03-09 21:18:06 +0700

    capitalize: change toBe to toMatch

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,5 +1,9 @@
 const capitalize = require("./capitalize.js")
 
 test('monyet to Monyet', () => {
-  expect(capitalize("monyet")).toBe("Monyet")
+  expect(capitalize("monyet")).toMatch("Monyet")
+})
+
+test('kumpulan to Kumpulan', () => {
+  expect(capitalize("kumpulan")).toMatch("Kumpulan")
 })
